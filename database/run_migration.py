ed7de063cc98eef38f143d2b5a37ea6f44f35c76
b4its <119672470+b4its@users.noreply.github.com>
2026-09-06 03:48:28 +0800

    fix(db): support execution inside docker container for run_migration.py

--- database/run_migration.py
+++ database/run_migration.py
@@ -7,6 +7,7 @@ Jalankan setelah container db siap:
 import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "backend"))
+sys.path.insert(0, "/app")
 
 from app.database import SessionLocal
 from sqlalchemy import text
